f3290cbb19df1ebd0f7d3954bd069f7de3fac1d4
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-05-15 15:26:05 +0200

    refactoring - common "starting point" for two functionalities

--- viral_seq_fetcher/__main__.py
+++ viral_seq_fetcher/__main__.py
@@ -23,13 +23,14 @@
 import logging
 import argparse
 import socket
+import sys
 
 from Bio import Entrez
 from src.findingRecords import *
 from src.SeqContainer import Container
 from src.commonFunctions import createDirIfNotExists, dateTime
 
-log_filename = os.path.join(os.path.dirname(__file__), CONF['log_file'])
+log_filename = os.path.join(os.path.dirname(__file__), CONF['log_file']) #TODO do zmiany, ma ścieżki unix'owe
 createDirIfNotExists(os.path.dirname(os.path.abspath(log_filename)))
 
 logging.basicConfig(level=logging.DEBUG,
@@ -38,7 +39,8 @@ logging.basicConfig(level=logging.DEBUG,
                     filename=log_filename,
                     filemode='w')
 
-if __name__ == "__main__":
+
+def main(args=sys.argv[1:]):
     logger = logging.getLogger(os.path.basename(__file__))
     parser = argparse.ArgumentParser(description='Short sample description')
     parser.add_argument('--email', action="store", required=True)
@@ -50,7 +52,7 @@ if __name__ == "__main__":
     parser.add_argument('--container', action="store", default='container_' + dateTime() + '.dump')
     # "d" stands for "debug"
     parser.add_argument('-d', action="store_true", default=False)
-    result = parser.parse_args()
+    result = parser.parse_args(args)
     # print result
     Entrez.email = result.email
     timeout = result.timeout
@@ -87,6 +89,36 @@ if __name__ == "__main__":
     seq_representations.extend(findHost(term, ids, out_dir, debug, tax_directory=CONF['taxonomy_dir']))
     createDirIfNotExists(out_dir)
     Container(seq_representations).save(container_path)
+
+
+def main_for_host_processing(args=sys.argv[1:]):
+    from src.hostProcessing import getAllHosts, putHostsInDb
+    parser = argparse.ArgumentParser(description='Short sample description')
+    parser.add_argument('--email', action="store")
+    parser.add_argument('--timeout', action="store", default=10, type=int)
+    parser.add_argument('--taxdir', action="store",
+                        # default=os.path.join(os.path.split(os.path.dirname(__file__))[0], 'files') \
+                        default = 'files'
+                        , help='directory for taxonomy files')
+    result = parser.parse_args(args)
+    tax_dir = result.taxdir
+    if not os.path.exists(tax_dir):
+        os.makedirs(tax_dir)
+    Entrez.email = result.email
+
+    timeout = result.timeout
+    socket.setdefaulttimeout(timeout)
+    hosts = getAllHosts(tax_dir)
+    putHostsInDb(hosts, True)
+
+
+if __name__ == "__main__":
+    print sys.argv
+    if sys.argv[0] == "blah":
+        pass
+    elif sys.argv[0] == "blah2":
+        pass
+    main()
     # TODO niech loguje ID sekwencji, z którymi się coś nie udało
     # TODO niech (może przy pierwszym wywołaniu programu, na poczśtku) uzupełnia bazę hostów. - doing
     # TODO - sprawdzić, czy potem w miarę szybko jest dostęp do tych danych
